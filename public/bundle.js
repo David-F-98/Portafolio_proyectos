843fd26655ab2a8a35e5952d454f7d16442b87f5
David-F-98 <u2903149@unimilitar.edu.co>
2024-01-14 10:50:05 -0500

    Animacion Burbujas

--- public/bundle.js
+++ public/bundle.js
@@ -29,5 +29,8 @@ const animarTexto = (texto)=>{
 window.addEventListener('load', async()=>{
     await animarTexto(document.querySelector('.hero__titulo--uno'));
     await animarTexto(document.querySelector('.hero__titulo--dos'));
+    
+    document.querySelectorAll('.hero__burbuja')[0].classList.add('hero__burbuja--active-1');
+    document.querySelectorAll('.hero__burbuja')[1].classList.add('hero__burbuja--active-2');
 });
 //# sourceMappingURL=bundle.js.map
--- src/index.js
+++ src/index.js
@@ -4,4 +4,7 @@ import animarTexto from "./animarTexto";
 window.addEventListener('load', async()=>{
     await animarTexto(document.querySelector('.hero__titulo--uno'));
     await animarTexto(document.querySelector('.hero__titulo--dos'));
+    
+    document.querySelectorAll('.hero__burbuja')[0].classList.add('hero__burbuja--active-1');
+    document.querySelectorAll('.hero__burbuja')[1].classList.add('hero__burbuja--active-2');
 })
