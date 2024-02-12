bf60feaba7b4db1d1d530fa4eca4a43c10d3e1ac
David-F-98 <u2903149@unimilitar.edu.co>
2024-02-11 22:19:16 -0500

    run dev

--- public/bundle.js
+++ public/bundle.js
@@ -179,4 +179,3 @@ window.addEventListener('load', async()=>{
     document.querySelectorAll('.hero__burbuja')[0].classList.add('hero__burbuja--active-1');
     document.querySelectorAll('.hero__burbuja')[1].classList.add('hero__burbuja--active-2');
 });
-//# sourceMappingURL=bundle.js.map
